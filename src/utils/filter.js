a1b6ff6f4859ed9369cc93ee99a360f24cf989a6
KyleNeedham <kylex691@hotmail.com>
2017-11-30 17:21:35 +0000

    Get empty chosen filters util

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -4,6 +4,13 @@ export function getChosenForGroup(chosen, group) {
   return chosen[group];
 }
 
+export function getEmptyChosen(available) {
+  return available.reduce((chosen, filter, i) => {
+    chosen[i] = filter.multiChoice ? [] : '';
+    return chosen;
+  }, {});
+}
+
 export function getQueryStringFromState(filterState) {
   return queryString.stringify(
     Object.keys(filterState.chosen).reduce((filters, chosen) => {
