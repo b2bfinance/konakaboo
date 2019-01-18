f63d47b2f1826f5fec4d503f9377901cac36ab52
KyleNeedham <git@kyleneedham.com>
2019-01-18 15:53:23 +0000

    Enforce button color

--- src/components/Product/ApplyButton.js
+++ src/components/Product/ApplyButton.js
@@ -3,6 +3,11 @@ import Fab from '@material-ui/core/Fab';
 
 export default styled(Fab)`
   background-color: ${props => props.theme.applyButtonBackground} !important;
+  color: ${props => props.theme.applyButtonColor};
   width: 100% !important;
   box-shadow: none !important;
+
+  &:hover {
+    color: ${props => props.theme.applyButtonColor};
+  }
 `;
