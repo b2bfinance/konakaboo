405fdc219a2b354017689e2daa83caa10a5f925e
KyleNeedham <git@kyleneedham.com>
2019-06-14 15:06:06 +0100

    Stub API

--- stubs/products/index.js
+++ stubs/products/index.js
@@ -1,15 +1,16 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
+const response = require('./response.json');
 
 const server = http.createServer(function(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.writeHead(200, { "Content-Type": "application/json" });
-  fs.createReadStream("response.json").pipe(res);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(response));
 });
 
-console.log("listening on 3000");
-server.listen(3000);
+console.log('listening on 4000');
+server.listen(4000);
