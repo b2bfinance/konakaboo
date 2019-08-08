b9ceccef2470358bad8e3763cbaef1738ae3dcf6
KyleNeedham <git@kyleneedham.com>
2019-08-08 16:23:46 +0100

    Comma seperate selected filter values

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,13 +1,16 @@
 import queryString from "query-string";
 
 export const makeFilterQueryString = filters => {
-  const queryObject = filters.reduce(
-    (selected, filter) => ({
-      ...selected,
-      [filter.key]: filter.selected,
-    }),
-    {}
-  );
+  const queryObject = filters.reduce((selected, filter) => {
+    if (filter.selected.length > 0) {
+      return {
+        ...selected,
+        [filter.key]: filter.selected.join(","),
+      };
+    }
+
+    return selected;
+  }, {});
 
   return queryString.stringify(queryObject);
 };
