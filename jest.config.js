f03e5af8c72f4010f2399ed1ee89f037c2c2a013
KyleNeedham <git@kyleneedham.com>
2021-04-15 10:19:30 +0100

    remove jest config

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect",
-  ],
-  verbose: true,
-  testRegex: "\\.(test|spec)\\.jsx?$",
-};
